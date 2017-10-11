f7cd79bcfd483d7e6832bd70117c694daf3279f5
Minde-PC\Minde <mindaugas.kurmauskas@gmail.com>
2017-10-11 22:08:47 +0300

    calc ndvi difference between spring/autumn and summer

--- request.py
+++ request.py
@@ -17,45 +17,42 @@ def band_image(band, date):
     response = requests.post(url = API_ENDPOINT, data = json.dumps(data))
     return rasterio.io.MemoryFile(response.content)
 
-dates = '2017-10-2'
+def get_ndvi(red_band, nir_band):
+    with rasterio.open(red_band) as red_raster_file:
+        with rasterio.open(nir_band) as nir_raster_file:
+            red = red_raster_file.read(1)
+            nir = nir_raster_file.read(1)
+            ndvi = (nir.astype(float) - red.astype(float)) / (nir + red)
+            ndvi[ndvi<0.5] = 0
+            ndvi[ndvi>0.8] = 0
 
-spring = '3-15'
-summer = '8-3'
-autumn = '10-3'
-red_band = band_image('04')
-nir_band = band_image('08')
+            return ndvi
 
-with rasterio.open(red_band) as red_raster_file:
-    with rasterio.open(nir_band) as nir_raster_file:
-        red = red_raster_file.read(1)
-        nir = nir_raster_file.read(1)
 
-        ndvi = (nir.astype(float) - red.astype(float)) / (nir + red)
-        ndvi[ndvi<0.5] = 0
-        ndvi[ndvi>0.8] = 0
+spring = '2017-3-16'
+summer = '2017-8-11'
+autumn = '2017-10-2'
+dates = [spring, summer, autumn]
 
-        profile = red_raster_file.meta
-        profile.update(driver='GTiff')
-        profile.update(dtype=rasterio.float32)
+ndvis = []
+for date in dates:
+    red_band = band_image('04', date)
+    nir_band = band_image('08', date)
 
-        with rasterio.open('thing.tif', 'w', **profile) as dst:
-            print(dst.shape)
-            print(ndvi.shape)
-            print(dst.indexes)
-            dst.write(ndvi.astype(rasterio.float32), 1)
+    ndvis.append(get_ndvi(red_band, nir_band))
 
-        # # ndvi = (nir.astype(float) - red.astype(float)) / (nir + red)
-        # print(ndvi.min(), ndvi.max())
-        # stretched_ndvi = (ndvi + 1) * 255
-        #
-        # kwargs = red_raster_file.meta
-        # kwargs.update(dtype = rasterio.uint8, count = 1)
-        #
-        # # profile = red_raster_file.meta
-        # # profile.update(driver='GTiff')
-        # # profile.update(dtype=rasterio.float32)
-        #
-        # with rasterio.open('out-ndvi.tif', 'w', **kwargs) as dst:
-        #     dst.write(stretched_ndvi.astype(rasterio.uint8))
+ndvi_avg = (ndvis[0] + ndvis[2])  / 2.0
 
-        print('succ')
+ndvi = ndvis[1] - ndvi_avg
+
+profile = band_image('04', '2017-3-16').meta
+profile.update(driver='GTiff')
+profile.update(dtype=rasterio.float32)
+
+with rasterio.open('thing.tif', 'w', **profile) as dst:
+    print(dst.shape)
+    print(ndvi.shape)
+    print(dst.indexes)
+    dst.write(ndvi.astype(rasterio.float32), 1)
+
+
